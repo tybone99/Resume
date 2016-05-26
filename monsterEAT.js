ca1a5f5164726053806269771523bba4bf68794d
Thornton <tybone99@gmail.com>
2016-05-25 22:26:43 -0600

    gatHUb

--- monsterEAT.js
+++ monsterEAT.js
@@ -1,12 +1,10 @@
 /**
  * Created by Thornton on 4/28/16.
  */
-// JavaScript Document
-
-
 // create an array of food items for the monster to eat
-var foodarr = ["Bubble Gum", "cotton Candy",  "Taffy" ,"salad" ,"carots" , "bannana"];
-var currentfood ="";
+var foodarr = ["Chocolate", "carrots", "Cotton Candy", "Apples", "broccoli", "Bananas", "Pizza", "popcorn", "salad"];
+
+
 // start the game with a function
 function rungame(){
     var thefood = getfood();
@@ -23,37 +21,36 @@ function getfood(){
 function setfood(food){
     currentfood = food;
     document.getElementById("foodbox").innerHTML = food;
-    return currentfood[randomfood];
+    return currentfood[randomfood]
 }
 
-function eatit() {
+function eatit(){
     //if the food is good
-    if (currentfood == "Bubble Gum" || currentfood == "Taffy" || currentfood == "Cotton Candy"){
-        document.getElementById("commentbox").innerHTML = "yummy";
-    var thefood = getfood();
-    setfood(thefood)
-}
-
-    // if the food is bad
-    else{
-        document.getElementById("commentbox").innerHTML = "yuk dont eat that";
-    }
-
-}
-
-function trashit() {
-    //if the food is good
-    if ( currentfood == "salad" || currentfood == "carots" ||  currentfood == "bannana") {
-    document.getElementById("commentbox").innerHTML = "yeah throw it out";
-}
-    // if the food is bad
-    else{
-        document.getElementById("commentbox").innerHTML = "no dont  trash that";
-        var thefood =getfood();
+    if(currentfood == "Chocolate" || currentfood == "popcorn" || currentfood == "Cotton Candy" || currentfood == "Pizza" || currentfood == "Bacon"){
+        document.getElementById("commentbox").innerHTML = " That's Delicious";
+        var thefood = getfood();
         setfood(thefood);
     }
+    // if the food is bad
+    else{
+        document.getElementById("commentbox").innerHTML = "What are you doing";
+
+
+    }
+
 }
 
+function trashit(){
+    //if the food is good
+    if(currentfood == "Chocolate" || currentfood == "popcorn" || currentfood == "Cotton Candy" || currentfood == "Pizza" || currentfood == "Bacon"){
+        document.getElementById("commentbox").innerHTML = "NO! don't throw that away";
 
+    }
+    // if the food is bad
+    else{
+        document.getElementById("commentbox").innerHTML = "Nice job that was gross";
+        var thefood = getfood();
+        setfood(thefood);
+    }
 
-                     
+}
